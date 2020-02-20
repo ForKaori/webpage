77119fb258a6d212931bd5fe4431e04e252c86c1
steph3ngu <>
2020-02-20 09:14:56 -0500

    add js file

---
+++ simple.js
@@ -0,0 +1,12 @@
+var toggleButton = document.querySelector("#content > button")
+var mainText = document.querySelector("h1#content")
+var isToggled = false
+
+toggleButton.addEventListener("click", function(){
+    if (isToggled){
+        document.querySelector("#content h1").innerHTML = "black";
+    } else {
+        document.querySelector("#content h1").innerHTML = "white";
+    }
+    isToggled = !isToggled
+})
